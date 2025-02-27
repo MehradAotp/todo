c4e56afaa02c6d83459e503f0a6da4e85a642d48
Mehrad Aotp <mehradaotp@gmail.com>
2025-02-27 11:11:32 +0330

    add mongodb Server

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,28 @@ import { TodoModule } from './todo/todo.module';
 import { TodoController } from './todo/todo.controller';
 import { TodoService } from './todo/todo.service';
 import { Todo, TodoSchema } from './todo/database/model';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Todo.name, schema: TodoSchema }]),
-    MongooseModule.forRoot('mongodb://localhost:27017/todos'),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        uri:
+          configService.get<string>('MONGO_URL') ||
+          `mongodb://${configService.get('MONGODB_HOST')}:${configService.get('MONGODB_PORT')}`,
+        user: configService.get<string>('MONGODB_USER'),
+        pass: configService.get<string>('MONGODB_PASSWORD'),
+        dbName: configService.get<string>('MONGODB_DATABASE'),
+        authSource: configService.get<string>('MONGODB_AUTHSOURCE') || 'admin',
+        directConnection: true,
+      }),
+    }),
     TodoModule,
   ],
   controllers: [TodoController],
